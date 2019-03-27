a7dcd46b3a829b44507cfe2b37f09520eb3dde05
Edi Susanto <edsusantoo@gmail.com>
2019-03-27 11:07:58 +0700

    merapihkan package

--- app/src/main/java/com/edsusantoo/bismillah/belajarroom/ui/main/MainActivity.kt
+++ app/src/main/java/com/edsusantoo/bismillah/belajarroom/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.edsusantoo.bismillah.belajarroom
+package com.edsusantoo.bismillah.belajarroom.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.edsusantoo.bismillah.belajarroom.R
 
 class MainActivity : AppCompatActivity() {
 
