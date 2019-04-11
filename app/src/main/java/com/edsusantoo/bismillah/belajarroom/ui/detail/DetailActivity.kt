e9c1a63caf79c2956b31061498852a930b278b34
Edi Susanto <edsusantoo@gmail.com>
2019-04-11 09:04:35 +0700

    setenable editext

--- app/src/main/java/com/edsusantoo/bismillah/belajarroom/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/edsusantoo/bismillah/belajarroom/ui/detail/DetailActivity.kt
@@ -18,12 +18,12 @@ class DetailActivity : AppCompatActivity() {
         setDataIntent()
 
         img_edit.setOnClickListener {
-
+            setEnableEditText()
         }
 
     }
 
-    fun setDataIntent() {
+    private fun setDataIntent() {
         if (intent.extras != null) {
             val dataUser: User = intent?.extras?.getSerializable("datauser") as User
             edt_username.setText(dataUser.username)
@@ -32,5 +32,27 @@ class DetailActivity : AppCompatActivity() {
             edt_notelp.setText(dataUser.notelp)
             edt_alamat.setText(dataUser.alamat)
         }
+    }
+
+    private fun setEnableEditText() {
+        edt_username.isEnabled = true
+        edt_username.isFocusableInTouchMode=true
+        edt_username.isFocusable=true
+
+        edt_email.isEnabled = true
+        edt_email.isFocusableInTouchMode=true
+        edt_email.isFocusable=true
+
+        edt_password.isEnabled = true
+        edt_password.isFocusableInTouchMode=true
+        edt_password.isFocusable=true
+
+        edt_notelp.isEnabled = true
+        edt_notelp.isFocusableInTouchMode=true
+        edt_notelp.isFocusable=true
+
+        edt_alamat.isEnabled = true
+        edt_alamat.isFocusableInTouchMode=true
+        edt_alamat.isFocusable=true
     }
 }
